a3cf24060640ea4fde2b5e2b271a9de70beffc69
Pavel Savchenko <87237353+pawelsawczenko@users.noreply.github.com>
2023-04-18 15:17:12 +0300

    Add fix for .card-selected bug

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -3,20 +3,17 @@ import { Suit } from "./Suit";
 import { gwentStore } from "../stores/GameStore";
 import React from "react";
 
-export type HandCellProps = {
-  isClicked: boolean;
-  setIsClicked: (boolean: boolean) => {};
-} & CardData;
-
 export const Cell: React.FC<CardData> = (props: CardData) => {
+  const onCellClick = () => {
+    gwentStore.moveCard(props);
+  };
+
   if (props.card === undefined) {
     return (
       <div
         id={`cell-${props.x}-${props.y}`}
         className="empty-cell"
-        onClick={() => {
-          gwentStore.moveCard(props);
-        }}
+        onClick={onCellClick}
       ></div>
     );
   } else {
@@ -35,10 +32,17 @@ export const Cell: React.FC<CardData> = (props: CardData) => {
 
 export const HandCell: React.FC<CardData> = (props: CardData) => {
   const [isClicked, setIsClicked] = React.useState(false);
-  // todo: look into wierd bug with class selected sometimes not working
-  // propably because of gwentStore.chooseCard()
   let selected =
     isClicked && gwentStore.cardToPlay?.id === props.id ? "card-selected" : "";
+
+  const onCellClick = () => {
+    gwentStore.chooseCard(props);
+    if (gwentStore.cardToPlay?.id === props.id) {
+      setIsClicked(true);
+    } else {
+      setIsClicked(false);
+    }
+  };
 
   if (props.card === undefined) {
     return <div id={`cell-${props.x}-${props.y}`} className="empty-cell"></div>;
@@ -47,10 +51,7 @@ export const HandCell: React.FC<CardData> = (props: CardData) => {
       <div
         id={`cell-${props.x}-${props.y}`}
         className={`card ${props.card.suit} ${selected}`}
-        onClick={() => {
-          gwentStore.chooseCard(props);
-          setIsClicked(!isClicked);
-        }}
+        onClick={onCellClick}
       >
         <span className="rank rank-before">{props.card.rank}</span>
         <Suit suit={props.card.suit} rank={props.card.rank}></Suit>
